9079cfbe5c8e7bffcd82535f162ca1340e9f2891
Joe <93642070+Dr-R0BOT@users.noreply.github.com>
2021-12-12 02:14:04 -0800

    updated 12/12/2021

---
+++ offsets.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Includes.h"
+
+namespace offsets
+{
+	std::uintptr_t cengine = 0x123f690;
+	std::uintptr_t cinput = 0x1d0a930;
+	std::uintptr_t cl_entitylist = 0x1971e08;
+	std::uintptr_t client_render_targets = 0x1fbb2c0;
+	std::uintptr_t client_state = 0x130c3f0;
+	std::uintptr_t cnetwork_u64_table = 0x750f360;
+	std::uintptr_t cprediction = 0x1fbaad0;
+	std::uintptr_t global_vars = 0x130c100;
+	std::uintptr_t glow_color = 0x1d0;
+	std::uintptr_t glow_enable = 0xf30;
+	std::uintptr_t glow_type = 0x2c0;
+	std::uintptr_t ibase_client_dll = 0x19588e0;
+	std::uintptr_t iclient_entity_list = 0x1fbaac8;
+	std::uintptr_t inetwork_string_table = 0x7511ba8;
+	std::uintptr_t iswapchain = 0xb932a08;
+	std::uintptr_t iv_debug_overlay = 0x75acd88;
+	std::uintptr_t iv_engine_client = 0x0;
+	std::uintptr_t iv_engine_trace = 0x7511b98;
+	std::uintptr_t iv_model_info_client = 0x7511b90;
+	std::uintptr_t iv_model_render = 0x1238490;
+	std::uintptr_t iv_render_view = 0x7511b80;
+	std::uintptr_t level_name = 0x130c5a0;
+	std::uintptr_t local_entity_handle = 0x1233038;
+	std::uintptr_t local_player = 0x1d21668;
+	std::uintptr_t name_list = 0xba17f40;
+	std::uintptr_t sign_on_state = 0x130c488;
+	std::uintptr_t vgui_system = 0x1f56bd0;
+	std::uintptr_t view_matrix = 0x1d4f430;
+	std::uintptr_t view_render = 0x7511b88;
+};
